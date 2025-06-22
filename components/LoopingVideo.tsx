becf36e25936cd652eb48e01da4345bd4f17d173
shishir <shishirtech506@gmail.com>
2025-06-22 16:34:13 +0530

    final commit

--- components/LoopingVideo.tsx
+++ components/LoopingVideo.tsx
@@ -7,11 +7,17 @@ interface LoopingVideoProps {
 
 const LoopingVideo: React.FC<LoopingVideoProps> = ({
   src = '@/public/videos/intro.mp4',
-  className = 'w-full max-w-[1000px] h-[200px] sm:h-[350px] md:h-[450px] lg:h-[550px] object-cover rounded-lg',
+  className = 'w-full h-auto max-w-[900px] object-contain',
 }) => {
   return (
-    <div className='bg-black flex items-center justify-center py-4 sm:py-8'>
-      <video autoPlay loop muted playsInline className={className}>
+    <div className='w-full flex justify-center items-center'>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className={`rounded-2xl${className}`}
+      >
         <source src={src} type='video/mp4' />
         Your browser does not support the video tag.
       </video>
